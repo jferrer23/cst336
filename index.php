98477011eb5d65ae04f2d59b7ba8078f351e465b
Jason Ferrer <jferrer@csumb.edu>
2017-09-05 02:23:49 +0000

    Styling

--- index.php
+++ index.php
@@ -1,12 +1,1 @@
 <?php header( 'Location: /index.html' ) ;  ?>
-<html>
-    <body>
-        <h1>Welcome to CST336 Workspace</h1>
-        <ul>
-            <h2>Labs:</h2>
-            <li>
-                <a href="lab1/index.html">Lab 1</a>
-            </li>
-        </ul>
-    </body>
-</html>
