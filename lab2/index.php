1a6b74210b4f74b880cd5a172de3732ec025b59b
Jason Ferrer <jferrer@csumb.edu>
2017-09-12 21:47:09 +0000

    Setting up lab 2

--- lab2/index.php
+++ lab2/index.php
@@ -14,26 +14,26 @@
             $sevenUrl = "./img/seven.png";
             $barUrl = "./img/bar.png";
             
-            $randVal = rand(0, 6);
+            $randVal = rand(0, 5);
             
             switch($randVal) {
                 case 0:
                     echo "<img src='$cherryUrl'>";
                     break;
                 case 1:
-                    echo "<img src='$cherryUrl'>";
+                    echo "<img src='$grapeUrl'>";
                     break;
                 case 2:
-                    echo "<img src='$cherryUrl'>";
+                    echo "<img src='$lemonUrl'>";
                     break;
                 case 3:
-                    echo "<img src='$cherryUrl'>";
+                    echo "<img src='$orangeUrl'>";
                     break;
                 case 4:
-                    echo "<img src='$cherryUrl'>";
+                    echo "<img src='$sevenUrl'>";
                     break;
                 case 5:
-                    echo "<img src='$cherryUrl'>";
+                    echo "<img src='$barUrl'>";
                     break;
             }
 
