b4c3e705b30291a127073a6a11b379ced7611600
Jason Ferrer <jferrer@csumb.edu>
2017-09-14 02:13:25 +0000

    Finishing up

--- lab2/index.php
+++ lab2/index.php
@@ -12,9 +12,9 @@
             <h1>Generic Slot Machine</h1>
             <br />
             <h3>Prizes</h3>
-            <h4>3 cherries = 100pts     3 grapes = 100pts     3 lemons = 100pts</h4>
-            <h4>3 oranges  = 250pts     3 bars   = 250pts</h4>
-            <h4>JACKPOT 3 sevens = 1000pts<h4>
+            <h4>3cherries= 100pts     3grapes= 100pts     3lemons= 100pts</h4>
+            <h4>3oranges= 250pts     3bars= 250pts</h4>
+            <h4>JACKPOT 3sevens= 1000pts<h4>
         </div>
         
         <div id="main">
