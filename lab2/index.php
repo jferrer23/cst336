bc55b1085e37edced34aff30f9be9e301bfa2f34
Jason Ferrer <jferrer@csumb.edu>
2017-09-14 01:29:33 +0000

    Adding Points

--- lab2/index.php
+++ lab2/index.php
@@ -35,6 +35,38 @@
                 return $imgUrl;
             }
             
+            function displayPoints($img1, $img2, $img3) {
+                echo "<div id='output'>";
+                if($img1 == $img2 && $img2 == $img3) {
+                    switch ($img1) {
+                        case "./img/cherry.png":
+                            $totalPoints = 100;
+                            break;
+                        case "./img/grapes.png":
+                            $totalPoints = 100;
+                            break;
+                        case "./img/lemon.png":
+                            $totalPoints = 100;
+                            break;
+                        case "./img/orange.png":
+                            $totalPoints = 250;
+                            break;
+                        case "./img/seven.png":
+                            echo "<h1>Jackpot!</h1>";
+                            $totalPoints = 1000;
+                            break;
+                        case "./img/bar.png":
+                            $totalPoints = 250;
+                        break;
+                    }
+                    else {
+                        echo "<h3>Try Again</h3>";
+                    }
+                
+                }
+                echo "</div>";
+            }
+            
             $imgUrl1 = getRandomImgUrl();
             $imgUrl2 = getRandomImgUrl();
             $imgUrl3 = getRandomImgUrl();
@@ -43,6 +75,7 @@
             echo "<img src='" .$imgUrl2."'>"; //id='reel2' 
             echo "<img src='" .$imgUrl3."'>"; //id='reel3' 
             
+            displayPoints($imgUrl1, $imgUrl2, $imgUrl3);
         
 
         ?>
